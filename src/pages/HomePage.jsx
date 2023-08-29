9198cd800f13b64e4fe88e899dc9d2e84293c100
kyleharris007 <buccolt45@yahoo.com>
2023-08-29 10:39:56 -0400

    updating navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
-  return(
+  return (
     <div>
       <h1>Home Page</h1>
     </div>
-  )
+  );
 }
 
 {/* <Link to="/memes/:memeId">Meme</Link>; */}
