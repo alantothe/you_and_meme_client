f5c84774f78a2eca394da969b88806034a024512
danish <danishwho_@Nashras-MacBook-Air.local>
2023-08-30 11:23:30 -0400

    worked on functionality for create meme page

--- src/pages/CreateMemePage.jsx
+++ src/pages/CreateMemePage.jsx
@@ -1,11 +1,89 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { memes } from "../assets/templates.js";
+import axios from "axios";
 
 function CreateMemePage() {
-  return <div></div>;
+  const oneMeme = memes[0];
 
+  const initialTextFields = {};
+  for (let i = 0; i < oneMeme.box_count; i++) {
+    initialTextFields[`text${i}`] = "";
+  }
+
+  const [formData, setFormData] = useState({
+    template_id: oneMeme.id,
+    username: "alantothe",
+    password: "Fresh1260!",
+    ...initialTextFields,
+  });
+
+  const [newMeme, setNewMeme] = useState(null);
+
+  const handleInputChange = (index, event) => {
+    const updatedFormData = {
+      ...formData,
+      [`text${index}`]: event.target.value,
+    };
+    setFormData(updatedFormData);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const formParams = new URLSearchParams(formData).toString();
+
+      const response = await axios.post(
+        "https://api.imgflip.com/caption_image",
+        formParams,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      setNewMeme(response.data.data.url);
+    } catch (error) {
+      console.error("Error sending request:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create Meme Page</h1>
+      <div className="w-1/4 mb-12 mt-12 flex flex-col items-center">
+        <div className=" w-60 h-60 relative">
+          {newMeme ? (
+            <img
+              className="absolute top-0 left-0 w-full h-full object-contain"
+              src={newMeme}
+              alt="Generated Meme"
+            />
+          ) : (
+            <img
+              className="absolute top-0 left-0 w-full h-full object-contain"
+              src={oneMeme.url}
+              alt={oneMeme.name}
+            />
+          )}
+        </div>
+
+        <div>
+          {Array.from({ length: oneMeme.box_count }).map((_, index) => (
+            <div key={index}>
+              <h1>Box {index + 1}</h1>
+              <input
+                type="text"
+                value={formData[`text${index}`]}
+                placeholder={`Enter text for Box ${index + 1}`}
+                onChange={(e) => handleInputChange(index, e)}
+              />
+            </div>
+          ))}
+          <button onClick={() => handleSubmit()}>Generate Meme</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-<Link to="/meme-selection">Meme Selection</Link>;
 
 export default CreateMemePage;
