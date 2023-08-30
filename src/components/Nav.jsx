f1201c3ef6e47887e5f238bec70148fc822826de
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-30 14:39:34 -0400

    corrects routes with nav-bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,6 +15,7 @@ import {
 
 import {
   UserCircleIcon,
+  UserPlusIcon,
   ChevronDownIcon,
   Cog6ToothIcon,
   PowerIcon,
@@ -81,6 +82,20 @@ function AccountMenu({user, handleLogOut }) {
               strokeWidth: 2,
             })}
             Edit Account
+          </MenuItem>
+        </Typography>)}
+
+        {!user && (<Typography as="a" href="/register">
+          <MenuItem
+            onClick={closeMenu}
+            className={"flex items-center gap-2 rounded"}
+            style={{ color: "rgb(96, 20, 30)" }}
+          >
+            {createElement(UserPlusIcon, {
+              className: "h-4 w-4",
+              strokeWidth: 2,
+            })}
+            Sign Up
           </MenuItem>
         </Typography>)}
 
