713e4b7f887fcd2d13fde5432053b56e076b232c
Rebekah <rebekahmenn@gmail.com>
2023-08-31 14:51:54 -0400

    home page text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,9 +15,12 @@ function HomePage() {
 
   return (
     <div className="bg-gray-800 min-h-screen">
-      <h1 className="text-center mb-8 font-bold text-4xl mt-10">
-        Welcome! 
+      <h1 className="text-center p-4 mb-4 font-bold text-4xl">
+        Welcome to You & Meme! 
       </h1>
+      <p className="text-center mb-8 font-bold text-2xl">
+        A place to share & create your favorite memes!
+      </p>
       <div className="flex flex-wrap px-48 items-center justify-center">
         {allPosts.map((allPosts, index) => (
           <SmallPostDetail allPosts={allPosts} key={index} />
