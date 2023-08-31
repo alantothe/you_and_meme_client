ed179f6014a116efa14ac5652f426a4ce16efd53
alantothe <rubenamalpartida@gmail.com>
2023-08-30 22:04:53 -0400

    token sent to local storage

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,12 @@
 import api from "./apiConfig";
+import jwtDecode from "jwt-decode";
 
 export const loginUser = async (loginData) => {
   try {
     const response = await api.post("/user/login/", loginData);
-    console.log(response.data);
-    return response.data;
+    localStorage.setItem("token", response.data.token);
+    const user = jwtDecode(response.data.token);
+    return user;
   } catch (err) {
     throw err;
   }
