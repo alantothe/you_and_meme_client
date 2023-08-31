be1d8a7e729dec494a5d9f70ad08a414ada22301
Rebekah <rebekahmenn@gmail.com>
2023-08-31 14:10:57 -0400

    div spacing on smallDetailPost

--- src/components/SmallPostDetail.jsx
+++ src/components/SmallPostDetail.jsx
@@ -4,12 +4,9 @@ function SmallPostDetail({ allPosts }) {
   console.log(allPosts.meme)
 
   return (
-  <div className="w-60 h-60 p-4 shadow-lg rounded-lg cursor-pointer bg-gray-400">
+  <div className="w-60 h-60 p-4 m-4 shadow-lg rounded-lg cursor-pointer bg-gray-400">
     <div className="flex flex-col items-center justify-around h-full w-full">
-    <img className="w-60 h-60 mt-4 rounded-lg" src={allPosts.meme} />
-        <p className="text-2xl font-bold text-blue-900">
-          {/* src={allPosts.meme} /> */}
-          </p>
+    <img className="object-contain w-full h-full mt-4" src={allPosts.meme} />
       </div>
   </div>
   )
