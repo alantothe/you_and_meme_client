0a105cbdac0cefd44d64d36f7237d4c51b2c3b66
kyleharris007 <buccolt45@yahoo.com>
2023-09-03 07:41:49 -0400

    updated sign-in page. Stuck on bg color and centering user and login in a column with tailwinds

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -28,39 +28,41 @@ function LoginPage() {
   };
 
   return (
-    <div className="login-form">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label className="text-gray-100 font-bold" htmlFor="username">
-          Username
-        </label>
-        <input
-          className="border-2 border-teal-500 rounded-md bg-teal-50"
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
+    <div className="page-container bg-gray-600">
+      <div className="login-form mt-2 flex justify-center items-center bg-gray-600">
+        {/* <h2 className="mb-2 text-2xl" variant="h4" color="blue-gray">Login</h2> */}
+        <form className="flex gap-6"onSubmit={handleSubmit}>
+          <label className="flex-col text-gray-100 font-bold" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="border-2 border-teal-500 rounded-md bg-teal-50"
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
 
-        <label className="text-gray-100 font-bold" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="border-2 border-teal-500 rounded-md bg-teal-50"
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
+          <label className="text-gray-100 font-bold" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="border-2 border-teal-500 rounded-md bg-teal-50"
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
 
-        <button color="teal" type="submit">
-          Login
-        </button>
-      </form>
+          <button className="hover:text-teal-500 rounded-full" color="teal" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
